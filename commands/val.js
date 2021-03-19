e19b1827107357b89e90ec21252ea2346c73f0df
Glitch (hello-express) <none>
2021-03-19 15:49:32 +0000

    📡🐸 Checkpoint ./commands/val.js:3369113/2666

--- commands/val.js
+++ commands/val.js
@@ -3,9 +3,17 @@ module.exports = {
 	description: 'looking for game command',
 	execute(message, args) {
 		message.channel.send('Pong.');
-    message.react("✅");
-    message.channel.awaitReactions((reaction, user) => {
-      user.id == message.author.id && (reaction.emoji.name == '✅')
+    message.react("✅")
+    
+    const filter = (reaction, user) => {
+	    return ['✅'].includes(reaction.emoji.name) && user.id === message.author.id;
+    };
+    
+    message.awaitReactions(filter, {max: 1, time: 12000}).then(collected => {
+      const reaction = collected.first();
+      console.log(reaction.users.cache)
+    }).catch(() => {
+      message.reply("make another queue request")
     })
 	},
 };
