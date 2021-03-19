01a9bc2bd4a23878fa776f1022be5718a0b107ce
Glitch (hello-express) <none>
2021-03-19 22:48:54 +0000

    👣🌕 Checkpoint ./commands/set.js:3369113/585

--- commands/set.js
+++ commands/set.js
@@ -1,7 +1,18 @@
+let config = require("./../config.js")
+
+let lfg = config["lfg-channel"]
+
+
+
 module.exports = {
 	name: "set",
 	description: 'set various properties',
 	execute(message, args) {
-	  console.log(args[0])
+	  if(/^<#.*>$/.test(args[0]) && args[0].length == 21) {
+      message.channel.send(`${args[0]} has been set to the active lfg channel. Any commands will not work outside of it.`)
+      console.log(lfg)
+    } else {
+      console.log("invalid channel name")
+    }
 	}
 };
