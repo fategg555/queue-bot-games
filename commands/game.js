7374e9651bebb3a3d1b08731a11cde8298df2b3b
Glitch (hello-express) <none>
2021-03-20 02:40:15 +0000

    💐🗾 Checkpoint ./commands/game.js:3369113/669 ./server.js:3369113/39 ./commands/add.js:3369113/4

--- commands/game.js
+++ commands/game.js
@@ -6,7 +6,13 @@ module.exports = {
 	name: "game",
 	description: 'remove yourself from the list of people for specific game queues',
 	execute(message, args) {
-    data[args[2]] = {stackSize: args[1], players: [], stack: [], name: args[0]}
+    try {
+      data[args[2]] = {stackSize: args[1], players: [], stack: [], name: args[0]}
+      message.reply(`The game ${args[10]} has been added with a maximum stack of ${args[1]}. You can request a queue/group for this game with the **qq ${args[2]}** command`)
+    } catch {
+      message.reply("Sorry, this game could not be added. Try again or consult qhelp.")
+    }
+    
     database.write(data)
 	}
 };
