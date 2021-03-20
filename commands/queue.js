67d6f96b2a670dee5fef0f8a9c8c23638f817585
Glitch (hello-express) <none>
2021-03-20 18:14:45 +0000

    🎰🚋 Checkpoint ./commands/queue.js:3369113/386

--- commands/queue.js
+++ commands/queue.js
@@ -17,9 +17,14 @@ module.exports = {
       message.reply("You are not in the LFG channel. Please enter commands into LFG or set the lfg channel with the qset <channel> command")
       return
     }
+    
+    if(!stack[message.guild.id][args[0]]) {
+      message.reply("This game does not currently exist on this server. Run **qgame** <name> <max number> <game shortcut> to create the game.")
+      return   
+    }
     stack[message.guild.id][args[0]].stack = [];
     message.channel.send(
-      `A ${stack[message.guild.id][args[0]].name} queue request has started. React to the msg above to secure a spot`
+      `A ${stack[message.guild.id][args[0]].name} queue request has started. React to the msg above to secure a spot.`
     );
     let people = "";
     for (let personID of stack[message.guild.id][args[0]].players) {
@@ -49,11 +54,14 @@ module.exports = {
         message.channel.send(`<@${user.id}>, you've already secured your spot!`)
         return
       }
+      if (user.tag == "Queuey Boi#6717") {
+        return
+      }
       message.channel.send(`Collected response from ${user.tag}`);
       stack[message.guild.id][args[0]].stack.push(user.id);
       
       if (stack[message.guild.id][args[0]].stackSize - stack[message.guild.id][args[0]].stack.length + 1 > 0) {
-        message.channel.send(`There are ${stack[message.guild.id][args[0]].stackSize - stack[message.guild.id][args[0]].stack.length + 1} spots remaining`);
+        message.channel.send(`There are ${stack[message.guild.id][args[0]].stackSize - stack[message.guild.id][args[0]].stack.length + 1} spots remaining.`);
       } else {
         let string = "";
         message.channel.send("There are no more spots remaining!");
@@ -61,7 +69,7 @@ module.exports = {
           string += `<@${id}> \n`;
         }
         message.channel.send(
-          `The final q for the stack is \n${string} \nThere will be more opportunities to queue in the future or you can start your own queue`
+          `The final q for the stack is \n${string} \nThere will be more opportunities to queue in the future or you can start your own queue.`
         );
       }
     });
