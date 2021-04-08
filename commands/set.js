9d18eac8da2efdd6ad5076032500a431822664eb
Glitch (hello-express) <none>
2021-04-08 17:08:16 +0000

    made set a universal command

--- commands/set.js
+++ commands/set.js
@@ -7,10 +7,10 @@ module.exports = {
 	description: 'set various LFG channel',
   params: "<none>",
 	async execute(message, args) {
-    if (args.length !== 2) {
-      message.channel.send("You don\'t have the right number of arguments. Make sure you have 2.")
-      return
-    }
+//    if (args.length !== 2) {
+  //    message.channel.send("You don\'t have the right number of arguments. Make sure you have 2.")
+    //  return
+    //}
     let server = await getGuildData(message.guild.name)
     let data = server[message.guild.id]
     if(!data["lfgs"]) {
@@ -24,19 +24,16 @@ module.exports = {
     if (!checkManager(message)) {
       message.channel.send("You do not have the *Game Manager* role. Either request to get the role or request someone with the role to set the LFG.")
       return
-      }
-
-    if (args[0] !== "lfg") {
-      message.channel.send(`Make sure that you're typing ${"`"}qset lfg <game code>${"`"}.`)
-      return
     }
     
-    if (!data[args[1]]) {
+   /* if (!data[args[1]]) {
       message.reply("This game does not exist. Set an lfg for existing games.")
       return
-    }
-	  if(message.channel.id.length == 18) {
-      // console.log("bruh why", await getGuildData(message.guild.name))
+    }*/
+
+    if (args[0] === "lfg") {
+	server = await getGuildData(message.guild.name)
+if (args.length !== 2) {message.reply("You need 2 arguments for this command to work"); return}
       if (server[message.guild.id][args[1]]["lfg"] !== message.channel.id) {
         if (!lfgs.includes(message.channel.id))lfgs.push(message.channel.id)
         await writeToGuild(message.guild, `${args[1]}.lfg`, message.channel.id)
@@ -61,19 +58,33 @@ module.exports = {
         return
       }
 
-      // let data = await getGuildData(message.guild.name)
-
       if (server[message.guild.id][args[1]]["lfg"] === message.channel.id) {
         message.channel.send("This channel has already been set.")
         return
       }
-      
-      // await writeToGuild(message.guild, "lfg", message.channel.id)
-     
-        
+    } else if (args[0] === "expiry") {
+if(args.length !== 3) {message.reply("You need 3 args for this command"); return}
+      try {
+let time = parseInt(args[1])
+        if (typeof(time) !== "number") {
+          message.reply("The value for the expiry is not valid. Make it is a WHOLE NUMBER.")
+          return
+        }
+        server = await getGuildData(message.guild.name)
+        game = server[message.guild.id][args[2]]
+        if (!game) {
+          message.reply("This game is invalid. Use `qview` to find the list of game codes.")
+          return
+        }
+        await writeToGuild(message.guild, `${args[2]}.queueTimeoutMins`, time)
+        message.channel.send(`The queue expiry time has been changed to **${time}** mins for **${game.name}**.`)
 
+      } catch (e) {
+        message.channel.send("An error occurred in setting the expiry time. Make sure your arguments are valid.")
+        return
+      }
     } else {
-      message.channel.send("This is an invalid channel. Try again.")
+      message.channel.send("Hmmmmmm..... either the setting you're using doesn't exist yet or is invalid.")
       return
     }
 	},
